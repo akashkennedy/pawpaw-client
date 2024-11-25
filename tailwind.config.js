cd7a97f208bbb56c00df265dffa99bcab41e5d27
Akash Kennedy <akashkennedy1@gmail.com>
2024-11-25 11:01:28 +0530

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B0A1BA",
+      },
+      borderRadius: {
+        pill: "16px",
+      },
+    },
   },
   plugins: [],
 };
